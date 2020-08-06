643a8a16c1b365dd182aad32fe5fee901d25d2c8
marianabastarracheas@gmail.com <marianabastarrachea@MacBook-Pro-2.local>
2020-08-05 21:32:12 -0500

    segundo commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const router = require("../routes");
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const cors = require ('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,7 @@ app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
 }))
+app.use (cors())
 app.use("/api/v1", router);
 
 module.exports = { app, PORT };
--- services/userServices.js
+++ services/userServices.js
@@ -17,3 +17,4 @@ module.exports = {
     return bcrypt.compareSync(candidatePassword, userPassword);
   }
 };
+
